d206ceedf3869b0b82dd0b7c65c89edd79e31de1
Jack Dempsey <jdempsey1993@gmail.com>
2019-09-19 22:33:15 -0400

    update

--- server.js
+++ server.js
@@ -14,3 +14,16 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+//// Router code
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+/// Adding listner to start server
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
