da7d37fb61c297d4fce26f46585257cc21b29517
Jack Dempsey <jdempsey1993@gmail.com>
2019-09-19 22:47:43 -0400

    Updated friends.js with friend array and export table array code

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+/// Data for to hold friends
+
+
+var friendArray = [
+    {
+        name:"Bob",
+        photo:"",
+        scores:[
+            5,
+            3,
+            4,
+            2,
+            4,
+            1,
+            4,
+            3,
+            5,
+            2
+        ]
+    },
+    {
+        name:"Sally",
+        photo:"",
+        scores:[
+            4,
+            3,
+            1,
+            4,
+            2,
+            4,
+            4,
+            2,
+            1,
+            3
+        ]
+    }
+]
+/// Export table array
+
+module.exports = tableArray
