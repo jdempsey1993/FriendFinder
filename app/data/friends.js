1a946dbd0ec5329e1965fc2148be7904cb444e87
Jack Dempsey <jdempsey1993@gmail.com>
2019-09-21 09:06:54 -0400

    Added more people to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -24,16 +24,81 @@ var friendArray = [
         scores:[
             4,
             3,
-            1,
+            5,
             4,
             2,
             4,
-            4,
+            5,
             2,
             1,
             3
         ]
     },
+    {
+        name:"Sam",
+        photo:"",
+        scores:[
+            3,
+            2,
+            3,
+            3,
+            5,
+            2,
+            3,
+            5,
+            4,
+            2
+        ]
+    },
+    {
+        name:"Joe",
+        photo:"",
+        scores:[
+            5,
+            2,
+            4,
+            3,
+            2,
+            4,
+            5,
+            3,
+            1,
+            3
+        ]
+    },
+    {
+        name:"Rachel",
+        photo:"",
+        scores:[
+            4,
+            4,
+            3,
+            1,
+            5,
+            3,
+            2,
+            4,
+            2,
+            4
+        ]
+    },
+    {
+        name:"Tom",
+        photo:"",
+        scores:[
+            3,
+            4,
+            5,
+            3,
+            4,
+            5,
+            3,
+            5,
+            1,
+            4
+        ]
+    },
+    
 ]
 /// Export table array
 
