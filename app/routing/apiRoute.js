732ef462ce45850cf6312d8e79753899cc8f7a8e
Jack Dempsey <jdempsey1993@gmail.com>
2019-11-21 20:28:37 -0500

    Updated friends.js

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -5,22 +5,21 @@ var tableData = require("../data/friends.js")
 
 // Routing 
 
-module.exports = function(app) {
-    
-    app.get("/friends/tables", function(req,res) {
-        res.json(tableData)
-    })
+module.exports = function (app) {
+
+        app.get("/api/friends", function (req, res) {
+            res.json(tableData)
+        })
 
 
-// Post Request 
+        // Post Request 
 
-app.post("/api/friends", function(req,rest){
-    if (tableData.length<5){
-        tableData.push(req.body)
-        res.json(true)
-    }
-    else {
-    tableData.push(req.body)
-    res.json(false)
-    }
-}
+        app.post("/api/friends", function (req, rest) {
+                    if (tableData.length < 5) {
+                        tableData.push(req.body)
+                        res.json(true)
+                    } else {
+                        tableData.push(req.body)
+                        res.json(false)
+                    }
+                }
