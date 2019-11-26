1cbc7ad13ec6467d3a38305db615543fb7cf598e
Jack Dempsey <jdempsey1993@gmail.com>
2019-11-26 18:21:33 -0500

    Fixed button in home.html to link to survey.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,23 +1,19 @@
 /// Require path
 
-var path = require("path")
+var path = require("path");
 
-/// Routing 
+/// Routing
 
 module.exports = function(app) {
+  /// HTML Get Requests
 
-    /// HTML Get Requests
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
 
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"))
-    })
+  // Default to home if no matching route is present
 
-    // Default to home if no matching route is present
-
-    app.get("*",function(req,res){
-        res.sendFile(path.join(__dirname,"../public/home.html"))
-    })
-}
-
-
-   
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
