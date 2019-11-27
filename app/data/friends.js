a1cd5b84c1fee00e1502943851d075b6a09429d7
Jack Dempsey <jdempsey1993@gmail.com>
2019-11-26 20:16:53 -0500

    Added pictures to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,32 +3,38 @@
 var friendArray = [
   {
     name: "Bob",
-    photo: "",
+    photo:
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpm1.narvii.com%2F5866%2F45cc61eb9e3eaefdc26be045e9f9fbfdf916f45b_hq.jpg&f=1&nofb=1",
     scores: [5, 3, 4, 2, 4, 1, 4, 3, 5, 2]
   },
   {
     name: "Sally",
-    photo: "",
+    photo:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2Foriginals%2Fd9%2F42%2F54%2Fd942542ce8300896cbc078eadeb88bd2.jpg&f=1&nofb=1",
     scores: [4, 3, 5, 4, 2, 4, 5, 2, 1, 3]
   },
   {
     name: "Sam",
-    photo: "",
+    photo:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wixstatic.com%2Fmedia%2F2d23a2_5cf9cb610460400e9098aee6c0017e75.jpg%2Fv1%2Ffill%2Fw_315%2Ch_469%2Cal_c%2Cq_80%2Cusm_0.66_1.00_0.01%2F2d23a2_5cf9cb610460400e9098aee6c0017e75.jpg&f=1&nofb=1",
     scores: [3, 2, 3, 3, 5, 2, 3, 5, 4, 2]
   },
   {
     name: "Joe",
-    photo: "",
+    photo:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FBduTxWnIUAAKT_5.jpg&f=1&nofb=1",
     scores: [5, 2, 4, 3, 2, 4, 5, 3, 1, 3]
   },
   {
     name: "Rachel",
-    photo: "",
+    photo:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2016%2F08%2Fcarrie-jean-435.jpg%3Fw%3D435&f=1&nofb=1",
     scores: [4, 4, 3, 1, 5, 3, 2, 4, 2, 4]
   },
   {
     name: "Tom",
-    photo: "",
+    photo:
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg.izismile.com%2Fimg%2Fimg3%2F20100428%2F640%2Fshe_makes_random_640_01.jpg&f=1&nofb=1",
     scores: [3, 4, 5, 3, 4, 5, 3, 5, 1, 4]
   }
 ];
